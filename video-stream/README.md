3a8a4c4865fe0adb255b88de3b179b7b8aef503a
Sarfarazjamal <jamalsarfaraz@gmail.com>
2020-07-27 12:02:32 +0400

    spring boot vedio stream initial commit

---
+++ video-stream/README.md
@@ -0,0 +1,19 @@
+VIDEO STREAM HTTP
+
+      Repository contains the code to stream the video in HTTP. Used the 
+      Spring fromework to Stream the Video
+      
+      Url endpoint is video/stream/mp4/toystory
+      Here the Path params are video/stream/{fileType}/{fileName}
+      
+      Applicaiton looks for the video in the resources/video folder, if want to
+      work on own video, place the file in video folder in resources and 
+      play around it.
+      
+      Start the applciation and hit the below url to try it out
+      http://localhost:8080/video/stream/mp4/toystory
+      
+      
+
+        
+      
---
+++ video-stream/src/main/java/com/jamal/videostream/controller/VideoStreamController.java
@@ -0,0 +1,31 @@
+package com.jamal.videostream.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.*;
+
+import com.jamal.videostream.service.VideoStreamService;
+
+import reactor.core.publisher.Mono;
+/**
+ * 
+ * @author JAMAL
+ *
+ */
+@RestController
+@RequestMapping("/video")
+public class VideoStreamController {
+
+    private final VideoStreamService videoStreamService;
+
+    public VideoStreamController(VideoStreamService videoStreamService) {
+        this.videoStreamService = videoStreamService;
+    }
+
+    @GetMapping("/stream/{fileType}/{fileName}")
+    public Mono<ResponseEntity<byte[]>> streamVideo(ServerHttpResponse serverHttpResponse, @RequestHeader(value = "Range", required = false) String httpRangeList,
+                                                    @PathVariable("fileType") String fileType,
+                                                    @PathVariable("fileName") String fileName) {
+        return Mono.just(videoStreamService.prepareContent(fileName, fileType, httpRangeList));
+    }
+}
